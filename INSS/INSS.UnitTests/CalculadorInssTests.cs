f5a5f8812c64e53d99d3637118ecf5bcdc31b0ba
Lucas Silva <luckkass89@gmail.com>
2021-02-16 18:28:09 -0300

    Testes de unidade

---
+++ INSS/INSS.UnitTests/CalculadorInssTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace INSS.UnitTests
+{
+    [TestClass]
+    public class CalculadorInssTests
+    {
+        CalculadorInss calculador;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            calculador = new CalculadorInss(new MockTabelaAliquotaRepository());
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DynamicData(nameof(DadosTeste))]
+        public void CalcularDesconto_TabelaAnualExiste_Sucesso(DateTime data, decimal salario, decimal descontoExperado)
+        {
+            var desconto = calculador.CalcularDesconto(data, salario);
+
+            Assert.AreEqual(descontoExperado, desconto);
+        }
+
+        [TestMethod]
+        public void CalcularDesconto_TabelaAnualNaoExiste_Erro()
+        {
+            try
+            {
+                var desconto = calculador.CalcularDesconto(new DateTime(9999, 1, 1), 100.00M);
+
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(typeof(System.NullReferenceException), ex.GetType());
+            }
+        }
+
+        public static IEnumerable<object[]> DadosTeste => new List<object[]>
+        {
+            new object[] { new DateTime(2011, 01, 01), 1000.00M, 80.00M },
+            new object[] { new DateTime(2011, 02, 05), 4000.00M, 405.86M },
+            new object[] { new DateTime(2011, 12, 05), 1500.00M, 135.00M },
+            new object[] { new DateTime(2012, 03, 07), 1000.00M, 70.00M },
+            new object[] { new DateTime(2012, 02, 10), 4000.00M, 440.00M },
+            new object[] { new DateTime(2012, 02, 10), 4100.00M, 500.00M }
+        };
+    }
+}
---
+++ INSS/INSS.UnitTests/MockTabelaAliquotaRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSS.UnitTests
+{
+    public class MockTabelaAliquotaRepository : ITabelaAliquotaRepository
+    {
+        private readonly TabelaAliquota[] _tabelas;
+
+        public MockTabelaAliquotaRepository()
+        {
+            var aliquotas2011 = new AliquotaPercentual[]
+            {
+                new AliquotaPercentual(0M, 1106.90M, 8.00M),
+                new AliquotaPercentual(1106.91M, 1844.83M, 9.00M),
+                new AliquotaPercentual(1844.84M, 3689.66M, 11.00M)
+            };
+            var teto2011 = 405.86M;
+            var tabelaAliquota2011 = new TabelaAliquota(2011, teto2011, aliquotas2011);
+
+            var aliquotas2012 = new AliquotaPercentual[]
+            {
+                new AliquotaPercentual(0M, 1000.00M, 7.00M),
+                new AliquotaPercentual(1000.01M, 1500.00M, 8.00M),
+                new AliquotaPercentual(1500.01M, 3000.00M, 9.00M),
+                new AliquotaPercentual(3000.01M, 4000.00M, 11.00M)
+            };
+            var teto2012 = 500.00M;
+            var tabelaAliquota2012 = new TabelaAliquota(2012, teto2012, aliquotas2012);
+
+            _tabelas = new TabelaAliquota[] { tabelaAliquota2011, tabelaAliquota2012 };
+        }
+
+        public TabelaAliquota ObterTabelaPorAno(int ano)
+        {
+            return _tabelas.FirstOrDefault(x => x.Ano == ano);
+        }
+    }
+}
